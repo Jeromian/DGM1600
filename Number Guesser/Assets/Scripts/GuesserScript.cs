5c974e53b4219f97540d83c5b6c4487399f32b92
Unknown <matthewgasser@outlook.com>
2018-01-17 09:53:33 -0700

    Added Key Inputs

--- Number Guesser/Assets/Scripts/GuesserScript.cs
+++ Number Guesser/Assets/Scripts/GuesserScript.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class GuesserScript : MonoBehaviour {
 
+        public int max;
+        public int min;
+    public int Guess;
+        
+
 	// Use this for initialization
-	void Start () {
-        print("You may begin!");
-	}
+	private void Start () {
+
+        
+
+        print("Welcome, you Fig Maggot!");
+        print("pick a number between "+min+" and "+max);
+
+        //Is value Guess
+        print("is it "+Guess+"?");
+        //push buttons up/down
+        print("press the up button for higher, the down button for lower, or the Spacebar to confirm.");
+
+       
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = Guess;
+            Guess = (min + max) / 2;
+            print("Is it" + Guess);
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = Guess;
+            Guess = (min + max) / 2;
+            print("Is it" + Guess);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            print("HAHA I KNEW IT WAS " + Guess+"!");
+
+        }
+
+    }
 }
