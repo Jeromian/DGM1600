7833cd7f31218d848cdc5aa8ffc89b88637ed5d3
Unknown <matthewgasser@outlook.com>
2018-01-26 16:43:24 -0700

    Finished Screen Printing and Debug

--- Text Adventure/Assets/Scripts/Adventure.cs
+++ Text Adventure/Assets/Scripts/Adventure.cs
@@ -12,6 +12,7 @@ public class Adventure : MonoBehaviour {
     public bool shiv;
     public bool podKey;
     public Text textObject;
+	public Text itemText;
 
 
 
@@ -34,6 +35,7 @@ public class Adventure : MonoBehaviour {
         if(currentState == States.cell)
         {
             Cell();
+			itemText.text = " ";
         }
 
         else if (currentState == States.window)
@@ -44,6 +46,7 @@ public class Adventure : MonoBehaviour {
         else if (currentState == States.bed)
         {
             Bed();
+			itemText.text = " ";
         }
 
         else if (currentState == States.door)
@@ -64,11 +67,13 @@ public class Adventure : MonoBehaviour {
 		else if (currentState == States.hall)
 		{
 			Hall();
+			itemText.text = " ";
 		}
 
 		else if (currentState == States.otherCell)
 		{
 			OtherCell();
+			itemText.text = " ";
 		}
 
 		else if (currentState == States.body)
@@ -154,7 +159,7 @@ public class Adventure : MonoBehaviour {
             else
             {
                 print("You dont have the CODE to open the Door");
-                textObject.text = "You dont have the CODE to open the Door";
+				itemText.text = "You dont have the CODE to open the Door";
             }
         }
         
@@ -162,10 +167,14 @@ public class Adventure : MonoBehaviour {
 
     private void Teddybear()
     {
-        print("Thw Teddybear is wearing a cute little labcoat and has a small tear in its neck.");
+        print("The Teddybear is wearing a cute little labcoat and has a small tear in its neck.");
+		textObject.text = "The Teddybear is wearing a cute little labcoat and has a small tear in its neck. /n" +
+			"Press B to go back to the Bed";
+
         if(doorCode == false)
         {
             print("OH!... You found a small paper with a code scrawled on it on the inside of the bears neck");
+			itemText.text="OH!... You found a small paper with a code scrawled on it on the inside of the bears neck";
             doorCode = true;
         }
         print("Press B to go back to the Bed");
@@ -176,6 +185,8 @@ public class Adventure : MonoBehaviour {
 	{
 		print("This mattres is hard as as a rock! OH... It is a rock.\n " +
 			"Press B to go back to the Bed.");
+		textObject.text = "This mattres is hard as as a rock! OH... It is a rock.\n " +
+		"Press B to go back to the Bed.";
 		if (Input.GetKeyDown(KeyCode.B)) { currentState = States.bed; }
 	}
 
@@ -183,6 +194,8 @@ public class Adventure : MonoBehaviour {
 	{
 		print("You're in a dimmly lit hall. You can go Left, Right, or enter the Other cell\n" +
 			"Press L for Left, R for Right, O for Other cell.");
+		textObject.text = "You're in a dimmly lit hall. You can go Left, Right, or enter the Other cell\n" +
+		"Press L for Left, R for Right, O for Other cell.";
 		if (Input.GetKeyDown(KeyCode.L)) { currentState = States.left; }
 		if (Input.GetKeyDown(KeyCode.R)) { currentState = States.right; }
 		if (Input.GetKeyDown(KeyCode.O)) { currentState = States.otherCell; }  
@@ -192,10 +205,16 @@ public class Adventure : MonoBehaviour {
 	{
 		print("Oh look, an escape Pod. \n " +
 			"Press H to go back to the Hall, P to enter the escape Pod.");
+		textObject.text = "Oh look, an escape Pod. \n " +
+		"Press H to go back to the Hall, P to enter the escape Pod.";
 		if (Input.GetKeyDown(KeyCode.H)) { currentState = States.hall; }
 		if (Input.GetKeyDown(KeyCode.P)) {
-			if (podKey == true) currentState = States.pod;
-			else print("You dont have the key to open the escape Pod");
+			if (podKey == true)
+				currentState = States.pod;
+			else {
+				print ("You dont have the key to open the escape Pod");
+				itemText.text = "You dont have the key to open the escape Pod";
+			}
 		}
 
 	}
@@ -204,10 +223,15 @@ public class Adventure : MonoBehaviour {
 	{
 		print("Theres an armed guard! But luckily his back is turned to you. \n " +
 			"Press S to go back to the Shank, R to enter the escape Rush.");
+		textObject.text = "Theres an armed guard! But luckily his back is turned to you. \n " +
+		"Press S to Shank him, R to Rush him, H to go back to the Hall.";
+		
 		if (Input.GetKeyDown(KeyCode.R)) { currentState = States.rush; }
+		if (Input.GetKeyDown(KeyCode.H)) { currentState = States.hall; }
 		if (Input.GetKeyDown(KeyCode.S)) {
-			if (podKey == true) currentState = States.shank;
+			if (shiv == true) currentState = States.shank;
 			else print("You dont have anything to Shank him with.");
+			itemText.text = "You dont have anything to Shank him with.";
 		}
 
 	}
@@ -216,27 +240,38 @@ public class Adventure : MonoBehaviour {
 	{
 		print("He Shot you!\n " +
 			"72 times!");
-		currentState = States.dead;
+		textObject.text = "He Shot you!\n " +
+		"72 times!\n " +
+		"PRESS C";
+		if (Input.GetKeyDown(KeyCode.C)) { currentState = States.dead; }
+
 	}
 
 	private void Dead()
 	{
 		print("GameOver");
+		textObject.text = "GameOver";
 	}
 
 	private void Shank()
 	{
 		print("HAHAHA! YOU GOT HIS POD KEY! ");
+		textObject.text = "HAHAHA! \n" +
+			"Press H to go back to the Hall";
 			podKey = true;
 		
 		print("Press H to go back to the Hall");
-		if (Input.GetKeyDown(KeyCode.B)) { currentState = States.bed; }
+		itemText.text = "YOU GOT HIS POD KEY!";
+		if (Input.GetKeyDown(KeyCode.H)) { currentState = States.hall; }
 	}
 
     private void OtherCell()
     {
         print("This cell is nearly identical to your own, but it's completely empty. Oh! Theres a dead Body in the middle of the floor!");
         print("Press B to check the Body, H to go back to the Hall");
+		textObject.text = "This cell is nearly identical to your own, but it's completely empty. \n" +
+			" Oh! Theres a dead Body in the middle of the floor! \n" +
+			"Press B to check the Body, H to go back to the Hall";
         if (Input.GetKeyDown(KeyCode.B)) { currentState = States.body; }
         if (Input.GetKeyDown(KeyCode.H)) { currentState = States.hall; }
     }
@@ -244,9 +279,12 @@ public class Adventure : MonoBehaviour {
     private void Body()
     {
         print("EEWWWW! This guy's been dead  for a while!");
+		textObject.text = "EEWWWW! This guy's been dead  for a while! \n" +
+			"Press O to go back to the Other cell";
         if (shiv == false)
         {
             print("OH!... YOU FOUND A SHIV in his ribcage. That's probably why he's dead.");
+			itemText.text = "OH!... YOU FOUND A SHIV in his ribcage. That's probably why he's dead.";
             shiv = true;
         }
         print("Press O to go back to the Other cell");
@@ -256,6 +294,7 @@ public class Adventure : MonoBehaviour {
     private void Pod()
     {
         print("YOU ESCAPED!");
+		textObject.text = "YOU ESCAPED!";
     }
 
 }
