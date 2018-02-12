85a72b8ffc4150ef8a16930f04ecb26d49e92218
Unknown <matthewgasser@outlook.com>
2018-02-12 09:57:40 -0700

    Added paddle

---
+++ BrickBreak/Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+
+	void Update ()
+    {
+        //get paddle position
+        Vector3 paddlePosition = new Vector3(0, gameObject.transform.position.y, 0);
+
+
+        //get mouse position
+        float mousePositionX = (Input.mousePosition.x / Screen.width * 16)-10;
+        //convert mouse position to world space
+        //save new position
+        paddlePosition.x = mousePositionX;
+        //set this paddle object to saved position
+        this.gameObject.transform.position = paddlePosition;
+	}
+}
