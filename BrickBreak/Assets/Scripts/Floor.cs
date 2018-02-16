e903ec9cd25189c78ddfa8ad0af9a41be1a9be9c
Unknown <matthewgasser@outlook.com>
2018-02-16 09:59:50 -0700

    Added GameOver and real sound

---
+++ BrickBreak/Assets/Scripts/Floor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+
+    public GameManager theGameManager;
+
+	
+	void Start ()
+    {
+        theGameManager = FindObjectOfType<GameManager>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("GAME OVER");
+        theGameManager.LoadLevel("GameOver");
+    }
+
+}
---
+++ BrickBreak/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)           //if instance is not assigned
+        {                               //then assign instance to this object
+            instance = this;
+        }
+
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);   //then destroy this object
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+
+    }
+
+
+}
